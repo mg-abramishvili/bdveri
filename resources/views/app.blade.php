45f6a80cd030c8e46bb48f03eb22315c65073a71
Mikhail A <mg@abramishvili.net>
2021-10-01 16:28:01 +0500

    .

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>bashdveri</title>
+        <title>БашДвери</title>
         <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet"/>
     </head>
     <body>
